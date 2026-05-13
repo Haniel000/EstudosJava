cf920e33eadf656c6f47f41546d58b362cf43463
Filipe <filipecouto33@gmail.com>
2026-05-13 10:31:16 -0300

    Enviando o Array05 Multidimensionais, especificando mais o nome dos arquivos, e formatando melhor os codigos.

--- Array/Array04.java
+++ Array/Array04.java
@@ -3,7 +3,8 @@ public class Array04 {
         int[] numeros = new int[2]; // Criando uma referencia e ja declarando o valor
         int[] numeros2 = {1,2,3,4,5}; //Criando a Array já definindo o tamanho só de inserir os valores
         int[] numeros3 = new int[]{5,4,3,2,1}; // Uma junção dos dois acima.
-
+        System.out.println(numeros[0]);
+        System.out.println(numeros2[0]); 
         //Foreach
         // É uma forma simplificada e melhorada de se escrever : 
         // int num = nuemros3[0]; 
