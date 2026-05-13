5f2180b8c455bbdf7416876149a074e892b0275d
Filipe <filipecouto33@gmail.com>
2026-05-13 09:41:19 -0300

    Enviando o Estudo da Aula04 de Array

---
+++ Array/Array04.java
@@ -0,0 +1,16 @@
+public class Array04 {
+    public static void main(String[] args) {
+        int[] numeros = new int[2]; // Criando uma referencia e ja declarando o valor
+        int[] numeros2 = {1,2,3,4,5}; //Criando a Array já definindo o tamanho só de inserir os valores
+        int[] numeros3 = new int[]{5,4,3,2,1}; // Uma junção dos dois acima.
+
+        //Foreach
+        // É uma forma simplificada e melhorada de se escrever : 
+        // int num = nuemros3[0]; 
+        // System.out.println(num);
+        for (int num : numeros3){
+            System.out.println(num);
+        }
+    }
+    
+}
